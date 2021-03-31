cf60bbd4ddb19000a910782b9575be2188832b80
TS <lisimah19@gmail.com>
2021-03-31 16:08:18 +0300

    Twicks to Version1

--- aipmap/sketch.js
+++ aipmap/sketch.js
@@ -2,32 +2,16 @@ let map;
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
-
-	if('geolocation' in navigator) {
-		console.log("location available");
-		navigator.geolocation.getCurrentPosition(position => {
-			console.log(position.coords);
-			const lat=position.coords.latitude;
-			const lon=position.coords.longitude;
-			document.getElementById('latitude').textContent=lat;
-			document.getElementById('longitude').textContent=lon;
-			const myLatLng = { lat: lat, lng: lon};
-			new google.maps.Marker({
-			  position: myLatLng,
-			  map,
-			  title: "Hello World!",
-			});
-			// var center = new google.maps.LatLng(lat,lon);
-            // map.panTo(center);
-			map.setCenter({
-				lat : lat,
-				lng : lon
-			});
-			map.setZoom(15);
-		  });
-	} else {
-		console.log("location not available");
-	}  
+	let userSettings = new Map([
+		['geolocation_option', 'on'],
+		['obstacles', 'off'],
+		['runway', 'off'],
+		['aerodromes', 'off'],
+		['aerodromeNear', 'on'],
+	]);
+	console.log(userSettings);
+	plotGeolocation(map,userSettings);
+ 
 }
 
 function draw() {
@@ -41,7 +25,4 @@ function initMap() {
 	zoom: 8,
   });
   console.log('Map is working');
-
-
-
 }
