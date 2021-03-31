1e4ba3237a1f805b9ff677e97ece77789d7cadb9
TS <lisimah19@gmail.com>
2021-03-31 11:49:47 +0300

    Version1: first implementation of Geolocation

--- aipmap/sketch.js
+++ aipmap/sketch.js
@@ -1,8 +1,47 @@
+let map;
+
 function setup() {
 	createCanvas(windowWidth, windowHeight);
+
+	if('geolocation' in navigator) {
+		console.log("location available");
+		navigator.geolocation.getCurrentPosition(position => {
+			console.log(position.coords);
+			const lat=position.coords.latitude;
+			const lon=position.coords.longitude;
+			document.getElementById('latitude').textContent=lat;
+			document.getElementById('longitude').textContent=lon;
+			const myLatLng = { lat: lat, lng: lon};
+			new google.maps.Marker({
+			  position: myLatLng,
+			  map,
+			  title: "Hello World!",
+			});
+			// var center = new google.maps.LatLng(lat,lon);
+            // map.panTo(center);
+			map.setCenter({
+				lat : lat,
+				lng : lon
+			});
+			map.setZoom(15);
+		  });
+	} else {
+		console.log("location not available");
+	}  
 }
 
 function draw() {
 	//background(225,0,225);
+	
+}
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+	center: { lat: 44.5, lng: 26 },
+	zoom: 8,
+  });
+  console.log('Map is working');
+
+
 
 }
