695b052c5517e3f2ede2cc5a476b8a5d23717285
Teodor Salomea <teodor.salomea@gmail.com>
2021-04-12 14:21:16 +0300

    Version_2-committed from Windows, code from Ubuntu

---
+++ aipmap/drawmap.js
@@ -0,0 +1,78 @@
+function plotGeolocation(map,usermap,latAp,lonAp){
+    if(usermap.get('geolocation_option')=='on'){
+        if('geolocation' in navigator) {
+            console.log("location available");
+            console.log(usermap.get('geolocation_option'));
+            navigator.geolocation.getCurrentPosition(position => {
+                console.log(position.coords);
+                const lat=position.coords.latitude;
+                const lon=position.coords.longitude;
+                document.getElementById('latitude').textContent=lat;
+                document.getElementById('longitude').textContent=lon;
+                document.getElementById('airportLat').textContent=latAp;
+                document.getElementById('airportLon').textContent=lonAp;
+
+                
+                var lineCoordinates = [
+                    {lat: lat, lng: lon},
+                    {lat: latAp, lng: lonAp},
+                ];
+                plotNearAirport(map,usermap,lineCoordinates);
+                addMarker(lat,lon);
+                var center = new google.maps.LatLng(lat,lon);
+                setCenterAndZoom(center,10);
+
+
+            });
+        }else {
+            console.log("location not available");
+        } 
+    }
+    else{
+        console.log("didnt happen");
+        console.log(usermap.geolocation_option);
+    }
+
+}
+
+function plotNearAirport(map,usermap,coords){
+    if(usermap.get('aerodromeNear')=='on'){
+        console.log('calling plotNearAirport');
+        addLine(coords);
+        setZoom(10);
+    }
+    
+}
+
+//Functions about map stuff TODO:put in separate file
+
+function addMarker(lat,lon){
+	console.log('adding Marker');
+    var center = new google.maps.LatLng(lat,lon);
+    new google.maps.Marker({
+      position: center,
+      map,
+      title: "Hello World!",
+    });
+}
+
+function addLine(coords){
+    console.log('adding Line');
+    var linePath = new google.maps.Polyline({
+        path: coords,
+        geodesic: true,
+        strokeColor: '#FF0000'
+        })
+    linePath.setMap(map);
+}
+
+function setCenterAndZoom(center,zoom){
+    map.setCenter(center);
+    map.setZoom(zoom);
+}
+
+function setZoom(zoom){
+    map.setZoom(zoom);
+}
+
+///////////////////////////////////////////////////////
--- aipmap/sketch.js
+++ aipmap/sketch.js
@@ -1,8 +1,44 @@
+let map;
+const api_url = 'https://api.flightplandatabase.com/nav/airport/lrop';
+
 function setup() {
 	createCanvas(windowWidth, windowHeight);
+	let userSettings = new Map([
+		['geolocation_option', 'on'],
+		['obstacles', 'off'],
+		['runway', 'off'],
+		['aerodromes', 'off'],
+		['aerodromeNear', 'on'],
+		['chosenAirportButton', 'LROP'],
+	]);
+	console.log(userSettings);
+	getAPI(map,userSettings);
 }
 
 function draw() {
 	//background(225,0,225);
-
+	
 }
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+	center: { lat: 44.5, lng: 26 },
+	zoom: 8,
+  });
+  console.log('Map is working');
+}
+
+async function getAPI(map,userSettings){
+    const response = await fetch(api_url);
+    const data = await response.json();
+    console.log("read from api the following JSON:")
+    console.log(data);
+
+    const {lat, lon}=data;
+	latAp=lat;
+	lonAp=lon;
+	
+	plotGeolocation(map,userSettings,latAp,lonAp);
+	addMarker(latAp,lonAp);
+}
+
