4f8978de2b109e8f372b7a9a6f5853e6fddfd091
Monica <andreea.scorta@gmail.com>
2021-04-28 11:08:08 +0300

    Monica5 commit

--- aipmap/drawmap.js
+++ aipmap/drawmap.js
@@ -80,6 +80,61 @@ function plotNearAirport(map,usermap,coords){
     
 }
 
+//--------------------------------------------------------------------MONICA
+
+function addNavaid(latnav,lonnav){
+    console.log('adding Navaid');
+    var position = new google.maps.LatLng(latnav,lonnav);
+    var image = new google.maps.MarkerImage(
+        'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.faa.gov%2Fabout%2Foffice_org%2Fheadquarters_offices%2Fang%2Foffices%2Ftc%2Flibrary%2Fstoryboard%2Fimg%2Fui%2FdetailedButtons%2Fnavaids_icon.png&f=1&nofb=1',
+        new google.maps.Size(71, 71),
+        new google.maps.Point(0, 0),
+        new google.maps.Point(17, 34),
+        new google.maps.Size(25, 25));
+    new google.maps.Marker({
+        position: position,
+        map,
+        icon: image,
+        title: "Naviad",
+    });
+
+}
+
+function addEnds(latends,lonends){
+    console.log('adding ends');
+    var position2 = new google.maps.LatLng(latends,lonends);
+    var image2 = new google.maps.MarkerImage(
+        'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ficon-library.com%2Fimages%2Frunway-icon%2Frunway-icon-4.jpg&f=1&nofb=1',
+        new google.maps.Size(71, 71),
+        new google.maps.Point(0, 0),
+        new google.maps.Point(17, 34),
+        new google.maps.Size(25, 25));
+    new google.maps.Marker({
+        position: position2,
+        map,
+        icon: image2,
+        title: "Runway",
+    });
+
+}
+
+
+function addRunwayPath(flightPlanCoordinates){
+    console.log('addRunwayPath');
+    console.log(flightPlanCoordinates);
+var RunwayPath = new google.maps.Polyline({
+    path: flightPlanCoordinates,
+    geodesic: true,
+    strokeColor: "#0055FF",
+    strokeOpacity: 1.0,
+    strokeWeight: 2,
+});
+RunwayPath.setMap(map);
+}
+
+
+//--------------------------------------------------------------------MONICA
+
 //Functions about map stuff TODO:put in separate file
 
 function addMarker(lat,lon){
--- aipmap/sketch.js
+++ aipmap/sketch.js
@@ -79,7 +79,8 @@ async function getAPI(map,userSettings,code,status) {
 				var latnav=obj.lat;
 				var lonnav=obj.lon;
 	
-				addMarker(latnav, lonnav);
+
+				addNavaid(latnav, lonnav);
 			}
 		}
 	}
@@ -89,8 +90,19 @@ async function getAPI(map,userSettings,code,status) {
 				var obj2=datarunway[k].ends[p];
 				var latends=obj2.lat;
 				var lonends=obj2.lon;
+				var obj3=datarunway[k].ends[p+1];
+				console.log(obj3);
+				var latends2=obj3.lat;
+				var lonends2=obj3.lon;
+
 	
-				addMarker(latends, lonends);
+				addEnds(latends, lonends);
+
+				var flightPlanCoordinates = [
+					{ lat: latends, lng: lonends },
+					{ lat: latends2, lng: lonends2 }
+				];
+				addRunwayPath(flightPlanCoordinates);
 			}
 		}
 	}
